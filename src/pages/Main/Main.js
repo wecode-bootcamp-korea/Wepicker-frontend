ce92d2b6b61072f2d2255f6063210e582caefd3a
kbhooo <ironman035614@gmail.com>
2021-01-12 23:04:59 +0900

    ADD: Main Page Layout 구현

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,11 +1,60 @@
 import React, { Component } from "react";
+import './Main.scss';
+import mainImg from "../../images/Main/main01.jpg";
 
-class Main extends Component {
+
+export class Main extends Component {
     render() {
-        return (
-            <div className="Main">
-                <h1>Main Page</h1>
-            </div>
+        return(
+
+            <section className="Main">
+
+                    <div className="mainBanner">
+                        <img 
+                        alt="Main Images" 
+                        className="mainImg" 
+                        src={ mainImg }/>
+                    </div>  
+
+
+                    <div className="centerBanner">
+                        <div className="midLeftBanner">
+                            <div className="centerBannerText">
+                                    <h1 className="centerEngTitle">
+                                    PACKAGE-FREE LIFESTYLE</h1>
+
+                                    <h2 className="centerKorTitle"> 
+                                    제로 웨이스트<br />
+                                    라이프스타일 플랫폼<br />
+                                    <br />
+                                    <br />
+                                    </h2>
+                                    <h5 className="storeLink">스토어 소개.</h5>
+                            </div>
+                        </div>
+
+                        <div className="midRightBanner" />
+
+                    </div>
+
+
+                    <div className="bottomBanner">
+
+                            <div className="bottomBannerText">
+                                <h4 className="subTitle">
+                                WE PICKER OFFLINE STORE</h4>
+
+                                <h1 className="bannerTitle"> 
+                                자연의 건강함을 위하여<br />
+                                지속가능한 소비문화 정착을<br />
+                                선릉역 매장에서 시작하세요.<br />
+                                </h1>
+                            </div>
+
+                    </div>
+
+            </section>
+
         );
     }
 }
