ecdfeaaf6f41033e805e8bebe5ac80660a50a50f
이새봄 <boma91@gmail.com>
2021-01-12 17:33:30 +0900

    Remove: eslintcache 제거

--- src/index.js
+++ src/index.js
@@ -2,4 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Routes from "./Routes";
 
+import "./styles/common.scss";
+import "./styles/reset.scss";
+
 ReactDOM.render(<Routes />, document.getElementById("root"));
