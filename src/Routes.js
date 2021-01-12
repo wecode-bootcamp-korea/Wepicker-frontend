f5a17bfebf156d7abfaa8918155ea07ac9c43636
kbhooo <ironman035614@gmail.com>
2021-01-12 22:59:39 +0900

    ADD: Nav Bar Layout 구현

--- src/Routes.js
+++ src/Routes.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Main from "./pages/Main/Main";
+import Nav from "./components/Nav/Nav"
 
 class Routes extends React.Component {
     render() {
@@ -9,6 +10,7 @@ class Routes extends React.Component {
             <Router>
                 <Switch>
                     <Route exact path="/" component={Main} />
+                    <Route exact path="/Nav" component={Nav} />
                 </Switch>
             </Router>
         );
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Routes from "./Routes";
+import './styles/reset.scss';
+import './styles/common.scss';
 
 ReactDOM.render(<Routes />, document.getElementById("root"));
