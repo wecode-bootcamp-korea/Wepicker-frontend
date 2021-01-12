cc1e7236080f05ebc5fb17c0855e5c2b735ebe40
이새봄 <boma91@gmail.com>
2021-01-12 23:46:53 +0900

    Modified: textarea 태그 JSX문법으로 변경

---
+++ src/pages/Agreement/Agreement.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+import "./Agreement.scss";
+
+class Agreement extends Component {
+    render() {
+        return (
+            <div className="Agreement">
+                <div className="checkFirst">
+                    <button></button>
+                    <span>이용약관 동의 (필수)</span>
+                </div>
+                <textarea value={0909090909090} />
+                <div className="checkSecond">
+                    <button></button>
+                    <span>개인정보 수집 및 이용 (필수)</span>
+                </div>
+                <textarea value={0909090909090} />
+            </div>
+        );
+    }
+}
+
+export default Agreement;
