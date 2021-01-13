a6488517eecebd6a6dc613911981d119229fb7e8
kbhooo <ironman035614@gmail.com>
2021-01-13 17:39:19 +0900

    MODIFY: 푸터 레이아웃 수정 중

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import './Footer.scss';
+import whiteLogo from "../../images/Footer/wepicker_logo_white.png"
 
 
 export class Footer extends Component {
@@ -9,38 +10,22 @@ export class Footer extends Component {
             <footer className="footerSection">
                 
                 <div className="footerLogo">
-                    <img alt="Wepicker White Logo" src="" >
+                    <img alt="Wepicker White Logo" src={whiteLogo} />
                 </div>
 
                 <div className="footerText">
                         <div className="infoText">
-                            <span>
+                            <span className="infoDetail">
                                 (주)위피커 &#124; 대표 : 정민선 이새봄 구보현 홍연우<br />
                                 사업자등록번호 : 777-22-210111 &#124; 통신판매업신고 : 2021-서울선릉-210111<br />
                                 개인정보보호 관리책임자 : 위피커 &#124; 고객센터 : 070-2021-0111 (월-금 08:00-12:00시)<br />
                                 이메일 : wepicker@wepicker.com &#124; 소재지 : 서울 강남구 테헤란로 427, 위워크타워 5F, 10F<br />
                                 Hosting by (주)아임웹
                             </span>
-                            <a>Terms of use</a>
-                            <span>&#124;</span>
-                            <a>Privacy Policy</a>
+
+
+                            
                         </div>
-
-                        <div className="menuText">
-
-                        </div>
-
-                        <div className="buttonSection">
-
-                        </div>
-                </div>
-
-                <div className="divideLine">
-                    
-                </div>
-
-                <div className="copyRight">
-
                 </div>
 
             </footer>
