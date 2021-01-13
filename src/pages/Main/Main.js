3191bf6eb9e48b68bf6bd4b7b6c1afd7f832bd14
kbhooo <ironman035614@gmail.com>
2021-01-13 15:56:59 +0900

    MODIFY: Effect React Library 추가

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import './Main.scss';
 import mainImg from "../../images/Main/main01.jpg";
+import Fade from 'react-reveal/Fade';
 
 
 export class Main extends Component {
@@ -16,47 +17,53 @@ export class Main extends Component {
                         src={ mainImg }/>
                     </div>  
 
+                    <Fade bottom>
+                            <div className="centerBanner">
+                                <div className="midLeftBanner">
+                                    <div className="centerBannerText">
+                                            <h1 className="centerEngTitle">
+                                            PACKAGE-FREE LIFESTYLE</h1>
 
-                    <div className="centerBanner">
-                        <div className="midLeftBanner">
-                            <div className="centerBannerText">
-                                    <h1 className="centerEngTitle">
-                                    PACKAGE-FREE LIFESTYLE</h1>
+                                            <h2 className="centerKorTitle"> 
+                                            제로 웨이스트<br />
+                                            라이프스타일 플랫폼<br />
+                                            <br />
+                                            <br />
+                                            </h2>
+                                            <h5 className="storeLink">스토어 소개.</h5>
+                                    </div>
+                                </div>
 
-                                    <h2 className="centerKorTitle"> 
-                                    제로 웨이스트<br />
-                                    라이프스타일 플랫폼<br />
-                                    <br />
-                                    <br />
-                                    </h2>
-                                    <h5 className="storeLink">스토어 소개.</h5>
+                                <div className="midRightBanner" />
+
                             </div>
-                        </div>
-
-                        <div className="midRightBanner" />
-
-                    </div>
+                    </Fade>
 
 
+                    
                     <div className="bottomBanner">
 
-                            <div className="bottomBannerText">
-                                <h4 className="subTitle">
-                                WE PICKER OFFLINE STORE</h4>
+                        <Fade bottom>
+                                <div className="bottomBannerText">
+                                    <h4 className="subTitle">
+                                    WE PICKER OFFLINE STORE</h4>
 
-                                <h1 className="bannerTitle"> 
-                                자연의 건강함을 위하여<br />
-                                지속가능한 소비문화 정착을<br />
-                                선릉역 매장에서 시작하세요.<br />
-                                </h1>
-                            </div>
+                                    <h1 className="bannerTitle"> 
+                                    자연의 건강함을 위하여<br />
+                                    지속가능한 소비문화 정착을<br />
+                                    선릉역 매장에서 시작하세요.<br />
+                                    </h1>
+                                </div>
+                        </Fade>
 
                     </div>
+                    
 
             </section>
 
         );
     }
 }
+  
 
 export default Main;
