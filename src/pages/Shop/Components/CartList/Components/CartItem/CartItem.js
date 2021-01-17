df049cafaffef9a2589c08f8e8d0016b855b459b
정민선 <minn602@gmail.com>
2021-01-17 14:24:53 +0900

    add: 장바구니 데이터 기능 구현중

--- src/pages/Shop/Components/CartList/Components/CartItem/CartItem.js
+++ src/pages/Shop/Components/CartList/Components/CartItem/CartItem.js
@@ -4,8 +4,18 @@ import './CartItem.scss'
 
 class CartItem extends Component {
   render() {
-    const {cartList, id, name, options, waysToPickup, delieveryFee} = this.props
-    console.log(options)
+    const {cartList, id, name, options, waysToPickup, delieveryFee} = this.props;
+
+    let totalQuantity = 0;
+    for(let i = 0; i < options.length; i ++) {
+      totalQuantity += options[i].quantity;
+    }
+
+    let totalPrice = 0;
+    for(let i = 0; i < options.length; i++) {
+      totalPrice += options[i].quantity * options[i].price;
+    }
+
     return(
       <tr className="CartItem">
         <td>
@@ -15,13 +25,20 @@ class CartItem extends Component {
           <div>
             <img alt="organic string bag" src="https://images.unsplash.com/photo-1537130508986-20f4fd870b4e?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=634&q=80" />
           </div>
-          <div>
-            <p>{name}</p>
-            <p>5개 / 2개</p>
-          </div>
+          <ul>
+            {
+              options && options.map((option) => {
+                return(
+                    <li>
+                      {option.name} | {option.quantity}개
+                    </li>  
+                )
+              })
+            }
+          </ul>
         </td>
         <td>
-          <p>2개</p>
+          <p>{totalQuantity}개</p>
           <button type="button">
             변경
           </button>
@@ -40,7 +57,7 @@ class CartItem extends Component {
           <p>3,000원</p>
         </td>
         <td>
-          8,000원
+          {totalPrice}
         </td>
         <td>
           <button className="deleteBtn" type="button">
