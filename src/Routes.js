7fd690e881475796fdbe03db27fe53227c0de5cf
kbhooo <ironman035614@gmail.com>
2021-01-18 10:56:29 +0900

    Modify: Side Menu Bar 구현완료

--- src/Routes.js
+++ src/Routes.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Nav from "./components/Nav/Nav"
+import Nav from "./components/Nav/Nav";
+import Sidebar from './components/Nav/SideBar';
 
 class Routes extends React.Component {
     render() {
@@ -9,6 +10,7 @@ class Routes extends React.Component {
             <Router>
                 <Switch>
                     <Route exact path="/" component={Nav} />
+                    <Route exact path="/Sidebar" component={Sidebar} />
                 </Switch>
             </Router>
         );
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import SideBar from './SideBar';
 import wepickerLogo from '../../images/Nav/wepicker_logo.png';
 import myPage from '../../images/Nav/human.png';
 import cart from '../../images/Nav/cart.png';
@@ -10,33 +11,44 @@ class Nav extends Component {
         super();
         this.state = {
             topLine: 'true',
-            bottomLine: 'true'
+            bottomLine: 'true',
+            sideMenuBar: 'true'
         };
     }
 
     handleToggleClick = () => {
         this.setState({
             topLine: !this.state.topLine,
-            bottomLine: !this.state.bottomLine
+            bottomLine: !this.state.bottomLine,
+            sideMenuBar: !this.state.sideMenuBar
         });
     };
 
     render() {
         return(
-            <nav className="navBar">
-                <div className="navContents">
-                        <div className="sideMenuBtn" onClick={this.handleToggleClick}>
-                            <span className={this.state.topLine ? 'topLineAct' : 'topLineInAct'}/>
-                            <span className={this.state.bottomLine ? 'bottomLineAct' : 'bottomLineInAct'}/>
+            <>
+                <div className="nav">
+                    <nav className="navBar">
+                        <div className="navContents">
+                                <div className="sideMenuBtn" onClick={this.handleToggleClick}>
+                                    <span className={this.state.topLine ? 'topLineAct' : 'topLineInAct'}/>
+                                    <span className={this.state.bottomLine ? 'bottomLineAct' : 'bottomLineInAct'}/>
+                                </div>
+                                <img alt="Main Logo" className="mainLogo"  src={wepickerLogo} />
+                                <div className="socialIcon">
+                                    <img alt="My Page Icon" className="myPageBtn"  src={myPage} />
+                                    <img alt="Cart Page Icon" className="cartPageBtn"  src={cart} />
+                                    <img alt="Wish List Page Icon" className="wishPageBtn"  src={wish} />
+                                </div>
                         </div>
-                        <img alt="Main Logo" className="mainLogo"  src={wepickerLogo} />
-                        <div className="socialIcon">
-                            <img alt="My Page Icon" className="myPageBtn"  src={myPage} />
-                            <img alt="Cart Page Icon" className="cartPageBtn"  src={cart} />
-                            <img alt="Wish List Page Icon" className="wishPageBtn"  src={wish} />
-                        </div>
+                    </nav>
                 </div>
-            </nav>
+
+                <div className={this.state.sideMenuBar ? 'hiddenSideBar' : 'showSideBar'}>
+                    <SideBar />
+                </div>
+
+            </>
         );
     }
 }
