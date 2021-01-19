414f457fd4304c0e48613bb82768c514c3d57dad
kbhooo <ironman035614@gmail.com>
2021-01-19 13:27:42 +0900

    Refactor: 수정요청사항 반영완료

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -9,53 +9,50 @@ import './Main.scss';
 export class Main extends Component {
     render() {
         return( 
-                <section className="Main">
-                    <Fade left>
-                        <div className="mainBanner">
-                            <img 
-                            alt="Main Images" 
-                            className="mainImg" 
-                            src={ mainImg }/>
-                        </div>
-                    </Fade>
+        <section className="Main">
+            <Fade left>
+                <div className="mainBanner">
+                    <img 
+                    alt="Main Images" 
+                    className="mainImg" 
+                    src={ mainImg }/>
+                </div>
+            </Fade>
+            <ScrollAnimation animateIn='fadeIn'>
+                <div className="centerBanner">
+                    <div className="midLeftBanner">
+                        <div className="centerBannerText">
+                            <h1 className="centerEngTitle">
+                            PACKAGE-FREE LIFESTYLE</h1>
 
-                    <ScrollAnimation animateIn='fadeIn'>
-                        <div className="centerBanner">
-                            <div className="midLeftBanner">
-                                <div className="centerBannerText">
-                                        <h1 className="centerEngTitle">
-                                        PACKAGE-FREE LIFESTYLE</h1>
-
-                                        <h2 className="centerKorTitle"> 
-                                        제로 웨이스트<br />
-                                        라이프스타일 플랫폼<br />
-                                        <br />
-                                        <br />
-                                        </h2>
-                                    <Link to="#">
-                                        <h5 className="storeLink">스토어 소개.</h5>
-                                    </Link>
-                                </div>
-                            </div>
-                
-                            <div className="midRightBanner" /> 
+                            <h2 className="centerKorTitle"> 
+                            제로 웨이스트<br />
+                            라이프스타일 플랫폼<br />
+                            <br />
+                            <br />
+                            </h2>
+                            <Link to="#">
+                                <h5 className="storeLink">스토어 소개.</h5>
+                            </Link>
                         </div>
-                    </ScrollAnimation>
-                            
-                    <div className="bottomBanner">
-                        <ScrollAnimation animateIn='fadeIn'>
-                                <div className="bottomBannerText">
-                                    <h4 className="subTitle">
-                                    WE PICKER OFFLINE STORE</h4>
-                                    <h1 className="bannerTitle"> 
-                                    자연의 건강함을 위하여<br />
-                                    지속가능한 소비문화 정착을<br />
-                                    선릉역 매장에서 시작하세요.<br />
-                                    </h1>
-                                </div>
-                        </ScrollAnimation>
                     </div>
-                </section>
+                    <div className="midRightBanner" /> 
+                </div>
+            </ScrollAnimation>
+            <div className="bottomBanner">
+                <ScrollAnimation animateIn='fadeIn'>
+                        <div className="bottomBannerText">
+                            <h4 className="subTitle">
+                            WE PICKER OFFLINE STORE</h4>
+                            <h1 className="bannerTitle"> 
+                            자연의 건강함을 위하여<br />
+                            지속가능한 소비문화 정착을<br />
+                            선릉역 매장에서 시작하세요.<br />
+                            </h1>
+                        </div>
+                </ScrollAnimation>
+            </div>
+        </section>
         );
     }
 }
